4251ace4a153c99d057b059e5156c218ec73f712
hyewon-p <hyewon.parkhw@gmail.com>
2024-02-26 14:07:37 +0900

    [fix] BOJ1987 주석 추가

--- hyewon-p/week5/BOJ1987.java
+++ hyewon-p/week5/BOJ1987.java
@@ -14,32 +14,44 @@ public class BOJ1987 {
 		FastReader fr = new FastReader();
 		R = fr.nextInt();
 		C = fr.nextInt();
+
+        // 알파벳 배열 보드 저장
 		board = new String[R];
+        // 보드의 칸에 방문했었는지 기록할 배열
 		visited = new boolean[R][C];
+        // 방문했던 알파벳 기록할 카운트배열
 		alphabets = new int[26];
 		for (int r = 0; r < R; r++) {
 			board[r] = fr.nextLine();
 		}
+
 		find(0, 0);
 		System.out.println(answer);
 	}
 
+    // 재귀로 상하좌우 모든 칸을 순회
 	static void find(int x, int y) {
+        // 만약 칸을 벗어나거나, 방문했었거나, 중복 알파벳이라면
+        // answer 갱신
 		if (!check(x, y) || visited[y][x] || alphabets[board[y].charAt(x) - 'A'] > 0) {
 			answer = Math.max(sum(alphabets), answer);
 			return;
 		}
+        
 		visited[y][x] = true;
 		alphabets[board[y].charAt(x) - 'A']++;
 		find(x + 1, y);
 		find(x, y + 1);
 		find(x - 1, y);
 		find(x, y - 1);
+
+        // 재귀이므로 값 돌려놓는 거 중요
 		alphabets[board[y].charAt(x) - 'A']--;
 		visited[y][x] = false;
 
 	}
 
+    // 보드 안에 있는 건지 검사
 	static boolean check(int x, int y) {
 		if (x < 0 || x > C - 1) {
 			return false;
@@ -50,6 +62,7 @@ public class BOJ1987 {
 		return true;
 	}
 
+    // 배열 합 구하는 함수
 	static int sum(int[] list) {
 		int total = 0;
 		for (int l : list) {
