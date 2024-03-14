5d752d13cd085188b776abbaf31c88d218f92fe3
sooa <sooa971@gmail.com>
2024-03-14 17:58:42 +0900

    [solved] BOJ1389

--- sooa971/week7/BOJ1389.java
+++ sooa971/week7/BOJ1389.java
@@ -2,43 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class BOJ1389 {
-	
-	static int n,m;
+
+	static int n, m;
 	static int[][] kb;
-	static boolean[][] know;
-	
+
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] line = br.readLine().split(" ");
 		n = Integer.parseInt(line[0]);
 		m = Integer.parseInt(line[1]);
-		
+
 		kb = new int[n][n];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				kb[i][j]=100;
-				if(i==j) {
-					kb[i][j]=0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				kb[i][j] = 100;
+				if (i == j) {
+					kb[i][j] = 0;
 				}
 			}
 		}
-		
-		for(int i=0; i<m; i++) {
+
+		for (int i = 0; i < m; i++) {
 			String[] line2 = br.readLine().split(" ");
-			kb[Integer.parseInt(line2[0])][Integer.parseInt(line2[1])]=1;
-			kb[Integer.parseInt(line2[1])][Integer.parseInt(line2[0])]=1;
+			kb[Integer.parseInt(line2[0])-1][Integer.parseInt(line2[1])-1] = 1;
+			kb[Integer.parseInt(line2[1])-1][Integer.parseInt(line2[0])-1] = 1;
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					
+					kb[i][j] = Math.min(kb[i][j],kb[i][k]+kb[k][j]);
+				}
+			}
+		}
+		int min =n*100;
+		int idx =0;
+		for(int i=0; i<n; i++) {
+			int sum=0;
+			for(int j=0; j<n; j++) {
+				sum+=kb[i][j];
+			}
+			if(sum<min) {
+				min=sum;
+				idx=i;
+			}
 		}
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				
-			}
-		}	
-	}
-	
-	public static void bfs(int r, int c) {
-		
+		System.out.println(idx+1);
 	}
 }
